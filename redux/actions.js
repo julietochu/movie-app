8f70d26e9ae432581b3748dd27269c8e1ad87406
juliet ochu <52784536+julietochu@users.noreply.github.com>
2022-05-05 11:40:42 -0600

    initial react native and redux setup

---
+++ redux/actions.js
@@ -0,0 +1,24 @@
+export const GET_MOVIES = 'GET_MOVIES';
+const API_URL = 'https://api.themoviedb.org/3/movie/popular';
+const API_KEY = 'ec2ed9910ad37b54fdf96f61311855a6';
+const PARAMS = 'page=1';
+const BASE_URL = `${API_URL}?api_key=${API_KEY}&${PARAMS}`;
+import axios from "axios";
+
+export const getMovies = () => {
+    try {
+      return async dispatch => {
+        const res = await axios.get(`${BASE_URL}`);
+        if (res.data) {
+          dispatch({
+            type: GET_MOVIES,
+            payload: res.data,
+          });
+        } else {
+          console.log('Unable to fetch');
+        }
+      };
+    } catch (error) {
+      // Add custom logic to handle errors
+    }
+  };
